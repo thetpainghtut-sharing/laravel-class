31cf417a5fff52998e053c5dd5d72bc6dbea56e7
TPH Sharing <thetpainghtut.sharing@gmail.com>
2025-07-01 22:10:42 +0630

    initial commit

---
+++ app/Http/Controllers/IntroController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class IntroController extends Controller
+{
+    public function show()
+    {
+        return view('showui');
+    }
+
+    
+}
