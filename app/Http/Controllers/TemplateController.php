735d75cc7e73fa72cd23851e13af354b9cdc3b84
TPH Sharing <thetpainghtut.sharing@gmail.com>
2025-07-04 22:11:17 +0630

    route and backend template

--- app/Http/Controllers/TemplateController.php
+++ app/Http/Controllers/TemplateController.php
@@ -6,6 +6,23 @@ use Illuminate\Http\Request;
 
 class TemplateController extends Controller
 {
+    public function index(){
+        $todos = [
+            [ 
+                "title" => 'Learn Laravel', 
+                "body" => 'Laravel is a web application framework with expressive, elegant syntax. Laravel is a tool to help you create strong, beautiful applications using the PHP programming language.'
+            ],
+            [
+                "title" => 'Learn Vue',
+                "body" => 'Vue is a progressive framework for building user interfaces.'
+            ],
+            [
+                "title" => 'Learn React',
+                "body" => 'React makes it painless to create interactive UIs.'
+            ]
+        ];
+        return view('mytemplate.index', compact('todos'));
+    }
     public function about(){
         return view('mytemplate.about');
     }
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// })->name('home');
+
 Route::get('/intros', [App\Http\Controllers\IntroController::class, 'show']);
 Route::get('/test', [App\Http\Controllers\IntroController::class, 'testfun']);
 // delete two routes (about, contact)
 
-Route::get('/about', [App\Http\Controllers\TemplateController::class, 'about']);
-Route::get('/post', [App\Http\Controllers\TemplateController::class, 'post']);
-Route::get('/contact', [App\Http\Controllers\TemplateController::class, 'contact']);
+
+Route::get('/', [App\Http\Controllers\TemplateController::class, 'index'])->name('homepage');
+Route::get('/about', [App\Http\Controllers\TemplateController::class, 'about'])->name('aboutpage');
+Route::get('/post', [App\Http\Controllers\TemplateController::class, 'post'])->name('postpage');
+Route::get('/contact', [App\Http\Controllers\TemplateController::class, 'contact'])->name('contactpage');
+
+// Backend
+Route::get('/table', [App\Http\Controllers\BackendTemplateController::class, 'tablefun'])->name('tablepage');
+
